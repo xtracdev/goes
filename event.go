147a382be70ccb36ac383db0c43cbe591cd015ef
d-smith <doug.smith.mail@gmail.com>
2016-08-02 07:41:43 -0700

    Updated comments on Event

--- event.go
+++ event.go
@@ -3,6 +3,10 @@ package goes
 //Event defines a structure that carries information related to an event, including the 
 //source aggregate of the event, the aggregate version the event is associated with, the payload, 
 //and the typecode indicating the type of the event.
+//
+//Note that events for an aggregate can be ordered by version; version is incremented for each event
+//associated with an aggregate. Event storage will also typically include a timestamp column for
+//the absolute ordering of events in terms of their storage date.
 type Event struct {
 	Source   string
 	Version  int
